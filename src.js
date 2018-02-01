8b55fba17f7f2868cd4e795e24b1c61cd3f38612
antsuma <anton.nordgren1@gmail.com>
2018-02-01 23:34:20 +0100

    asd

---
+++ src.js
@@ -0,0 +1,13 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC8L06cksUfIip-DEMbLc9Xgf2pnBSuoY8",
+    authDomain: "lab1-7fa59.firebaseapp.com",
+    databaseURL: "https://lab1-7fa59.firebaseio.com",
+    projectId: "lab1-7fa59",
+    storageBucket: "lab1-7fa59.appspot.com",
+    messagingSenderId: "619341925518"
+};
+firebase.initializeApp(config);
+
+console.log("Hello World");
