0a801efc629adbd697d4560477ef06ac7c568fcc
antsuma <anton.nordgren1@gmail.com>
2018-02-04 18:37:18 +0100

    asd

--- src.js
+++ src.js
@@ -183,10 +183,6 @@ logoutButton.addEventListener("click", function() {
 
 function logIn() {
     let user = prompt("Enter User");
-    localStorage.setItem('user', "Anton");
+    localStorage.setItem('user', user);
 }
-
-
-
-
 
